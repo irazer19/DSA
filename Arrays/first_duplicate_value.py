81033dcbd144244c380e397a95e17f4e65404808
irazer19 <irazer19@gmail.com>
2022-10-19 11:29:21 +0530

    added

---
+++ Arrays/first_duplicate_value.py
@@ -0,0 +1,31 @@
+"""
+Given an array of integers between 1 and n, inclusive, where n is the length of the array, write a function that
+returns the first integer that appears more than once.
+
+input = [2, 1, 5, 2, 3, 3, 4]
+Output: 2
+
+"""
+
+
+def firstDuplicateValue(array):
+    # Time: O(n) and Space: O(1)
+    """
+    Logic:
+    Since all the integers in the array are positive, and between 1 and n, inclusive.
+    As we iterate, we will mark the (current element value - 1) index of the array as negative.
+    This way, if we ever encounter an element with index (value - 1) which is already negative,
+    we will return that element.
+    """
+    # For every element.
+    for i in range(len(array)):
+        # We will reduce the element value by 1 because if the element val = n, then we dont have nth index in the
+        # array.
+        idxToCheck = abs(array[i]) - 1
+        # If this index element is already negative, we will return this element.
+        if array[idxToCheck] < 0:
+            return abs(array[i])
+        # Else, we will mark it as negative.
+        array[idxToCheck] *= -1
+    # Default return.
+    return -1
