a425f6605217bb56862bf6a59ec7eb930f292d78
irazer19 <irazer19@gmail.com>
2022-12-13 17:17:20 +0530

    added

--- Arrays/laptop_rentals.py
+++ Arrays/laptop_rentals.py
@@ -8,40 +8,50 @@ another student can rent the same laptop during any time interval starting with 
 Write a function that returns the minimum number of laptops that the school needs to rent such that all students will
 always have access! to a laptop when they need one.
 
+times = [
+          [0, 2],
+          [1, 4],
+          [4, 6],
+          [0, 4],
+          [7, 8],
+          [9, 11],
+          [3, 10]
+        ]
+Output: 3
 """
 
 
 def laptopRentals(times):
-    # Time: O(nlog(n)) and Space: O(n)
-    """
-    Logic:
-    Split the start and end times into two arrays. Now iterate through the start and end times using pointers,
-    If the start time >= end time, it means that one student has already finished using a laptop and another student
-    needs it.
-    Else if the start time < end time, it means that we need a new laptop.
+	# Time: O(nlog(n)) and Space: O(n)
+	"""
+	Logic:
+	Split the start and end times into two arrays. Now iterate through the start and end times using pointers,
+	If the start time >= end time, it means that one student has already finished using a laptop and another student
+	needs it.
+	Else if the start time < end time, it means that we need a new laptop.
 
-    """
+	"""
 
-    # Splitting the start and end times into two arrays and sorting them,
-    startTime = sorted([item[0] for item in times])
-    endTime = sorted([item[1] for item in times])
+	# Splitting the start and end times into two arrays and sorting them,
+	startTime = sorted([item[0] for item in times])
+	endTime = sorted([item[1] for item in times])
 
-    # Result
-    usedLaptops = 0
-    # Starting index for start and end times for iteration.
-    startIdx = 0
-    endIdx = 0
-    # Iterate until we have a start time.
-    while startIdx < len(times):
-        # If the start time >= end time, we reduce the used laptop count because a student has finished using it.
-        if startTime[startIdx] >= endTime[endIdx]:
-            usedLaptops -= 1
-            # We also increase the end time to get the next finish time.
-            endIdx += 1
-        # We always increment the used laptop count because we have a student who needs a laptop at the current
-        # start time.
-        usedLaptops += 1
-        # Moving to the next start time.
-        startIdx += 1
+	# Result
+	usedLaptops = 0
+	# Starting index for start and end times for iteration.
+	startIdx = 0
+	endIdx = 0
+	# Iterate until we have a start time.
+	while startIdx < len(times):
+		# If the start time >= end time, we reduce the used laptop count because a student has finished using it.
+		if startTime[startIdx] >= endTime[endIdx]:
+			usedLaptops -= 1
+			# We also increase the end time to get the next finish time.
+			endIdx += 1
+		# We always increment the used laptop count because we have a student who needs a laptop at the current
+		# start time.
+		usedLaptops += 1
+		# Moving to the next start time.
+		startIdx += 1
 
-    return usedLaptops
+	return usedLaptops
