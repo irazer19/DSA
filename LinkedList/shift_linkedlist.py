86d6a3757408f17c27583e4edba64767e409dcd2
saurav_gupta <saurav.gupta@redhuntlabs.com>
2023-10-19 18:29:17 +0530

    Updated shift_linkedlist.py

--- LinkedList/shift_linkedlist.py
+++ LinkedList/shift_linkedlist.py
@@ -24,42 +24,29 @@ def shiftLinkedList(head, k):
     To do that, we traverse the linked list at the k-1 node and then grab the new head and manipulate the pointers
     to point to the old head.
     """
-    # Counting the total number of nodes in the linked list.
-    totalNodes = getTotalNodes(head)
-    # Taking the mod of k for knowing the exact distance which will be between 0 to k-1.
-    k = k % totalNodes
-    # Edge case, if k is 0, then we just return the same list.
-    if k == 0:
-        return head
-    # Else we compute the distance to traverse from the head node, we want to reach the node which is before
-    # the new head that's why we add -1
-    nodesToTravel = totalNodes - k - 1
-
-    # variable for traversing.
+    # we move the node pointer to the k distance
     node = head
-    # Traversing the computed distance.
-    counter = 0
-    while counter != nodesToTravel:
+    while k > 0:
         node = node.next
-        counter += 1
-    # Grabbing the new head.
-    newHead = node.next
-    # Here we want to find the tail of the new head so that we can connect it to the older head.
-    newTail = newHead
-    while newTail.next:
-        newTail = newTail.next
-    # The first part of the original list becomes the tail, so we make its end point to None.
-    node.next = None
-    # Now the new tail if the last part of the original list which points to the head of the original list.
-    newTail.next = head
+        k -= 1
+    # Now we create new pointer variables, where first ptr is at 0th node and second ptr is at kth distance.
+    # We will move both the pointers until we the second ptr reaches the end so that we can divide the linkedlist
+    # into two havles such that to shift it.
+    second_ptr = node
+    first_ptr = head
+    while second_ptr.next:
+        first_ptr = first_ptr.next
+        second_ptr = second_ptr.next
+    # At this point, we get the new head of the list
+    newhead = first_ptr.next
+    # We find the old tail by which will now become the middle node of the list
+    curr = newhead
+    while curr.next:
+        curr = curr.next
+    oldtail = curr
+    # Connecting the old head to the older header
+    oldtail.next = head
+    # The first pointer is already at the node which will be out new tail so its next = None
+    first_ptr.next = None
 
-    return newHead
-
-
-def getTotalNodes(node):
-    # Function to count the total numbers of nodes in the list.
-    total = 0
-    while node:
-        node = node.next
-        total += 1
-    return total
+    return newhead
