d044fd86929dbd656f033f7d341a7a630cd1870f
Saurav Gupta <irazer19@gmail.com>
2024-12-20 18:40:27 +0530

    update

--- Arrays/caesar_cipher_encryptor.py
+++ Arrays/caesar_cipher_encryptor.py
@@ -6,25 +6,26 @@ strings= "xyz"
 key=2
 Output= "zab"
 
+Similar: https://leetcode.com/problems/shifting-letters/
 """
 
 
 def caesarCipherEncryptor(string, key):
-	# Time: O(n) and Space: O(n)
-	"""
-	Logic:
-	We will take the mod of the key, and the for every letter, we will add the key to its ordinal value.
-	We will check whether the ordinal value is within the alphabet range or greater than 122, and extract the
-	correct letter.
-	"""
-	# Mod of key
-	key = key % 26
-	result = []
-	for c in string:
-		# new ordinal value after adding the key
-		totalOrd = ord(c) + key
-		# If the new ordinal value is <= 122, then we are good.
-		# Else we will take the mod of 122, and then add the remainder to the starting letters ordinal value.
-		newCharOrd = totalOrd if totalOrd <= 122 else 96 + (totalOrd % 122)
-		result.append(chr(newCharOrd))
-	return ''.join(result)
+    # Time: O(n) and Space: O(n)
+    """
+    Logic:
+    We will take the mod of the key, and the for every letter, we will add the key to its ordinal value.
+    We will check whether the ordinal value is within the alphabet range or greater than 122, and extract the
+    correct letter.
+    """
+    # Mod of key
+    key = key % 26
+    result = []
+    for c in string:
+        # new ordinal value after adding the key
+        totalOrd = ord(c) + key
+        # If the new ordinal value is <= 122, then we are good.
+        # Else we will take the mod of 122, and then add the remainder to the starting letters ordinal value.
+        newCharOrd = totalOrd if totalOrd <= 122 else 96 + (totalOrd % 122)
+        result.append(chr(newCharOrd))
+    return "".join(result)
