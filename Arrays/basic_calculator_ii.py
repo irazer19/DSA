5e18e5ea95a12c9744afbaf8f956e767d4b78516
Saurav Gupta <irazer19@gmail.com>
2024-12-20 12:31:38 +0530

    update

--- Arrays/basic_calculator_ii.py
+++ Arrays/basic_calculator_ii.py
@@ -5,60 +5,54 @@ All intermediate results will be in the range of [-231, 231 - 1].
 
 Input: s = "3+2*2"
 Output: 7
+
+https://leetcode.com/problems/basic-calculator-ii/description/
+
 """
 
 
 def calculate(s: str) -> int:
-	# Time: O(n) and Space: O(1)
-	"""
-	Logic:
-	We iterate over all the characters, now we assume that the starting operator is +.
-	If the current char is a digit, then we use another while loop to parse all the digits to form a number.
-	Once we have a number, then we check what was is the current operator, depending on that we update the resultant
-	sum.
-	If the current char is not a digit but an operator, then we update the current operator.
-	"""
-	# To store the final result as we move.
-	currSum = 0
-	# Stores the previous number encountered
-	prev = 0
-	# Starting operator
-	op = '+'
-	# Starting from index 0
-	i = 0
+    # Time: O(n) and Space: O(1)
+    """
+    Brute force:
+    Process the string character by character and follow the standard order of operations (multiply/divide first, then add/subtract)
+    We use a stack to store the processed number.
+    Time: O(n) and Space: O(n)
 
-	while i < len(s):
-		# If the current char is a digit.
-		if s[i].isdigit():
-			# We will form a number and parse all the next digits
-			num = 0
-			# While the next char is also a digit.
-			while i < len(s) and s[i].isdigit():
-				# Parsing it to a number
-				num = num * 10 + int(s[i])
-				i += 1
-			# We decrement as we want to make the pointer to the last found digit.
-			i -= 1
+    Optimized:
+    The code processes the expression by scanning each character one by one. As it reads digits, it builds them into a complete number
+    (curr_num). When it encounters an operator or reaches the end of the string, it performs the previous operation stored in op.
 
-			# Now if the last seen operator was +, -, *, /
-			if op == '+':
-				currSum += num
-				prev = num
-			elif op == '-':
-				currSum -= num
-				prev = -num
-			elif op == '*':
-				# The logic is simple here,
-				currSum -= prev
-				currSum += num * prev
-				prev = num * prev
-			elif op == '/':
-				currSum -= prev
-				currSum += int(prev / num)
-				prev = int(prev / num)
-		# Now if the current char is not a digit and also not a whitespace, then it must be an operator.
-		# So we update the operator variable.
-		elif s[i] != ' ':
-			op = s[i]
-		i += 1
-	return currSum
+    """
+    s = s.strip()
+    # To store the final result as we move.
+    currSum = 0
+    # Stores the previous number encountered
+    prev = 0
+    # Starting operator
+    op = "+"
+    # Store the current number at hand
+    curr_num = 0
+
+    for i in range(len(s)):
+        # If the current char is a digit.
+        if s[i].isdigit():
+            curr_num = curr_num * 10 + int(s[i])
+        if s[i] in ["+", "-", "*", "/"] or i == len(s) - 1:
+            if op == "+":
+                currSum += curr_num
+                prev = curr_num
+            elif op == "-":
+                currSum -= curr_num
+                prev = -curr_num
+            elif op == "*":
+                currSum -= prev
+                currSum += curr_num * prev
+                prev = curr_num * prev
+            elif op == "/":
+                currSum -= prev
+                currSum += int(prev / curr_num)
+                prev = int(prev / curr_num)
+            op = s[i]
+            curr_num = 0
+    return currSum
