2996629a0f3182e0c412649540c83c58b7a3d695
Saurav Gupta <irazer19@gmail.com>
2024-12-20 17:41:41 +0530

    update

--- Arrays/best_time_buy_sell_stocks_2.py
+++ Arrays/best_time_buy_sell_stocks_2.py
@@ -10,21 +10,24 @@ Output: 7
 Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
 Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 Total profit is 4 + 3 = 7.
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+
 """
 
 
 def maxProfit(prices) -> int:
-	# Time: O(n) and Space: O(1)
-	"""
-	Logic:
-	We do the transaction on each day. We compare the current price with the previous day price, if its greater,
-	then we do the transaction and add the profit to the result.
-	"""
-	profit = 0
-	# We start from index 1, because on index 0, we cannot do any transaction.
-	for i in range(1, len(prices)):
-		# If the current day price is greater than previous day price
-		if prices[i - 1] < prices[i]:
-			# We do the transaction and add it to the profit.
-			profit += prices[i] - prices[i - 1]
-	return profit
+    # Time: O(n) and Space: O(1)
+    """
+    Logic:
+    We do the transaction on each day. We compare the current price with the previous day price, if its greater,
+    then we do the transaction and add the profit to the result.
+    """
+    profit = 0
+    # We start from index 1, because on index 0, we cannot do any transaction.
+    for i in range(1, len(prices)):
+        # If the current day price is greater than previous day price
+        if prices[i - 1] < prices[i]:
+            # We do the transaction and add it to the profit.
+            profit += prices[i] - prices[i - 1]
+    return profit
