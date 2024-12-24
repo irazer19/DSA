6c9679a0be35f1be53d02d6d6f2f3cc2db828ca3
Saurav Gupta <irazer19@gmail.com>
2024-12-24 09:12:02 +0530

    update

--- Arrays/longest_substring_without_duplicate.py
+++ Arrays/longest_substring_without_duplicate.py
@@ -4,40 +4,42 @@ Write a function that takes in a string and returns its longest substring withou
 string = "clementisacap",
 Output: "mentisac"
 
+https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+Brute Force:
+Generate every substring, and then check for the longest.
+Time: O(n^3) and Space: O(m), m is the size of the character set
+
+Optimized:
+The key insight for optimization is that when we find a repeating character, we can jump our start pointer directly
+to the position after its last occurrence, rather than checking all possible substrings.
+
+Time: O(n) and Space: O(m), m is the size of the character set
 """
 
 
 def longestSubstringWithoutDuplication(string):
-	# Time = Space = O(n)
-	"""
-	Logic:
-	We will use start, end pointers to track the length of the substring.
-	The end pointer will keep moving forward, and the start pointer will be updated if we see a duplicate
-	letter again, now either the duplicate letter appeared before the start of the current substring, or after the
-	start of the current substring.
-	In any case, we will always update the longest substring result as we move forward.
-	"""
-	# Start, end pointers for the substring.
-	start = 0
-	end = 0
-	# Store the longest substring result.
-	longest = [0, 1]
-	# Dictionary to store the last seen for the letters
-	lastSeen = {}
-	# We iterate to the end of the string.
-	while end < len(string):
-		# Getting the current letter.
-		letter = string[end]
-		# If the letter is already seen before
-		if letter in lastSeen:
-			# We update the start position of the substring.
-			# We take max(current start position, last seen letters next position)
-			start = max(start, lastSeen[letter] + 1)
-		# We always update the last seen of every letter in the dictionary
-		lastSeen[letter] = end
-		# Updating the result with max substring length.
-		longest = max(longest, [start, end], key=lambda x: x[1] - x[0])
-		# Moving forward.
-		end += 1
-	
-	return string[longest[0]: longest[1] + 1]
+    # Start, end pointers for the substring.
+    start = 0
+    end = 0
+    # Store the longest substring result.
+    longest = [0, 1]
+    # Dictionary to store the last seen for the letters
+    lastSeen = {}
+    # We iterate to the end of the string.
+    while end < len(string):
+        # Getting the current letter.
+        letter = string[end]
+        # If the letter is already seen before
+        if letter in lastSeen:
+            # We update the start position of the substring.
+            # We take max(current start position, last seen letters next position)
+            start = max(start, lastSeen[letter] + 1)
+        # We always update the last seen of every letter in the dictionary
+        lastSeen[letter] = end
+        # Updating the result with max substring length.
+        longest = max(longest, [start, end], key=lambda x: x[1] - x[0])
+        # Moving forward.
+        end += 1
+
+    return string[longest[0] : longest[1] + 1]
