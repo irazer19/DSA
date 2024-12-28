2655c03183446a76491714b016955f346f14914e
Saurav Gupta <irazer19@gmail.com>
2024-12-28 10:33:14 +0530

    update

--- Arrays/one_edit.py
+++ Arrays/one_edit.py
@@ -7,49 +7,55 @@ stringOne = 'hello'
 stringTwo = 'hollo'
 
 output: True
+
+https://leetcode.com/problems/one-edit-distance/description/
+
+Brute force:
+The brute force approach would be to try all possible edit operations (insert, delete, replace) at every position and
+check if we can transform one string into another with exactly one operation.
+Time: O(n^2) and Space: O(n)
+
+Optimized:
+We will iterate over the both the strings using pointers. If the chars dont match then we have 3 cases:
+1. if the length of stringone > string2, then we can either remove from stringone or add this char to stringtwo.
+2. Similarly if the length of stringtwo > stringone, we do the above operation.
+3. If the length of stringone == stringtwo, then we will swap the non-matching character with the matching character
+
+Now if the stringone == stringtwo, then we will simply move to the next characters.
+Time: O(n) and Space: O(1)
 """
 
 
 def oneEdit(stringOne, stringTwo):
-	# Time: O(n) and Space: O(1)
-	"""
-	Logic:
-	We will iterate over the both the strings using pointers. If the chars dont match then we have 3 cases:
-	1. if the length of stringone > string2, then we can either remove from stringone or add this char to stringtwo.
-	2. Similarly if the length of stringtwo > stringone, we do the above operation.
-	3. If the length of stringone == stringtwo, then we will swap the non-matching character with the matching character
+    # Edge case
+    if abs(len(stringOne) - len(stringTwo)) > 1:
+        return False
 
-	Now if the stringone == stringtwo, then we will simply move to the next characters.
-	"""
-	# Edge case
-	if abs(len(stringOne) - len(stringTwo)) > 1:
-		return False
+    idxOne = 0
+    idxTwo = 0
+    # Since we are allowed to make only 1 edit, we will use a boolean to store it.
+    madeEdit = False
 
-	idxOne = 0
-	idxTwo = 0
-	# Since we are allowed to make only 1 edit, we will use a boolean to store it.
-	madeEdit = False
+    while idxOne < len(stringOne) and idxTwo < len(stringTwo):
+        # If the current chars dont match in the string.
+        if stringOne[idxOne] != stringTwo[idxTwo]:
+            # IF we have already made the edit, retutn false
+            if madeEdit:
+                return False
+            # Else we will make the edit
+            madeEdit = True
+            # Whether length of stringOne >  stringTwo or vice-versa, we will can either remove or add
+            if len(stringOne) > len(stringTwo):
+                idxOne += 1
+            elif len(stringOne) < len(stringTwo):
+                idxTwo += 1
+            else:
+                # Else if the length if same, then we will swap, and move to the next char.
+                idxOne += 1
+                idxTwo += 1
+        else:
+            # Case where the chars are matching, then we will move to the next char for both the strings.
+            idxOne += 1
+            idxTwo += 1
 
-	while idxOne < len(stringOne) and idxTwo < len(stringTwo):
-		# If the current chars dont match in the string.
-		if stringOne[idxOne] != stringTwo[idxTwo]:
-			# IF we have already made the edit, retutn false
-			if madeEdit:
-				return False
-			# Else we will make the edit
-			madeEdit = True
-			# Whether length of stringOne >  stringTwo or vice-versa, we will can either remove or add
-			if len(stringOne) > len(stringTwo):
-				idxOne += 1
-			elif len(stringOne) < len(stringTwo):
-				idxTwo += 1
-			else:
-				# Else if the length if same, then we will swap, and move to the next char.
-				idxOne += 1
-				idxTwo += 1
-		else:
-			# Case where the chars are matching, then we will move to the next char for both the strings.
-			idxOne += 1
-			idxTwo += 1
-
-	return True
+    return True
--- Arrays/partition_labels.py
+++ Arrays/partition_labels.py
@@ -10,45 +10,46 @@ Explanation:
 The partition is "ababcbaca", "defegde", "hijhklij".
 This is a partition so that each letter appears in at most one part.
 A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+https://leetcode.com/problems/partition-labels/description/
+
+Brute Force:
+Find all the min and max index for every letter, and then merge overlapping intervals.
+Time: O(n) and Space: O(1) because we only store at most 26 characters
+
+Optimized:
+First, we create a dictionary that stores the last occurrence of each character in the string.
+
+We maintain two pointers:
+start: beginning of current partition
+end: tracks the furthest extending character in current partition
+
+As we iterate through the string:
+For each character, we update end to be the maximum of current end and the last occurrence of current character
+When our current index reaches end, we've found a valid partition
+Time Complexity: O(n)
+Space Complexity: O(1) since we only store at most 26 characters in the hash map
+
 """
 
 
 def partitionLabels(s):
-	# Time = Space = O(n)
-	"""
-	Logic:
-	We will first store all the max index of each letter in a hash table.
-	We will maintain a start and end window index.
-	Start window starts from 0, and then we compute the end window by getting the largest index of that letter
-	present in the string s.
-	Now since we have start & end window, we will loop over all letters within the window and update the end window
-	if one of the letter has larger index value.
-	Once we have looped over all the window elements, we will add the (end - start + 1) to the result, and now
-	the start window = end + 1
+    # Get the last occurrence of each character
+    last_occurrence = {char: i for i, char in enumerate(s)}
 
-	"""
-	# Get max index for each and every letter
-	idxTable = {}
-	for i in range(len(s)):
-		idxTable[s[i]] = max(idxTable.get(s[i], -1), i)
-	# Storing the result
-	result = []
-	# Start and end window index
-	start = 0
-	end = 0
-	while start < len(s):
-		# Getting the end window for the current characters max index found in the string s.
-		end = max(idxTable[s[start]], end)
-		# Now we iterate over all the elements between start and end window
-		j = start
-		while j < end:
-			# If the jth elements largest index is greater than the current end index, then we update it because
-			# we want all the occurrences of this element in the same window.
-			end = max(idxTable[s[j]], end)
-			j += 1
-		# Finally, we add to the result
-		result.append(end - start + 1)
-		# Now we update the start window = the next index after the current end window index.
-		start = end + 1
+    partitions = []
+    start = 0
+    end = 0
 
-	return result
+    # Iterate through the string
+    for i, char in enumerate(s):
+        # Update the end to be the furthest last occurrence
+        # of any character we've seen in current partition
+        end = max(end, last_occurrence[char])
+
+        # If we've reached the end of current partition
+        if i == end:
+            partitions.append(end - start + 1)
+            start = i + 1
+
+    return partitions
