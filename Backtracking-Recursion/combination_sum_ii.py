acd9857190831d25519b26c1e6f371d302615f6e
Saurav Gupta <irazer19@gmail.com>
2025-01-02 21:22:50 +0530

    update

--- Backtracking-Recursion/combination_sum_ii.py
+++ Backtracking-Recursion/combination_sum_ii.py
@@ -8,43 +8,45 @@ Input: candidates = [10,1,2,7,6,1,5], target = 8
 Output:
 [[1,1,6], [1,2,5], [1,7], [2,6]]
 
+https://leetcode.com/problems/combination-sum-ii/
+
 """
 
 
 def combinationSum2(candidates, target):
-	# Time: O(2^n) and Space: O(number of combinations + n)
-	"""
-	Logic:
-	We will either include an element or move to the next index without adding that element in the stack.
-	Because we can have duplicate elements, we will sort the input array, and for the second function call, we will
-	move to the index where is element is not the same as the current index element.
-	If the sum of the stack == target, we will the result and return.
+    # Time: O(2^n) and Space: O(number of combinations + n)
+    """
+    Logic:
+    We will either include an element or move to the next index without adding that element in the stack.
+    Because we can have duplicate elements, we will sort the input array, and for the second function call, we will
+    move to the index where is element is not the same as the current index element.
+    If the sum of the stack == target, we will the result and return.
 
-	"""
-	# Sorting the input array
-	candidates.sort()
-	# Result
-	result = []
-	solve(candidates, target, 0, [], result)
-	return result
+    """
+    # Sorting the input array
+    candidates.sort()
+    # Result
+    result = []
+    solve(candidates, target, 0, [], result)
+    return result
 
 
 def solve(candidates, target, idx, stack, result):
-	# Base case
-	if sum(stack) == target:
-		result.append(stack[:])
-		return
-	# Base case
-	if idx >= len(candidates) or sum(stack) > target:
-		return
-	# Getting the current element
-	currVal = candidates[idx]
-	# Adding this element to the stack and moving to the next index.
-	stack.append(currVal)
-	solve(candidates, target, idx + 1, stack, result)
-	stack.pop()
-	# Now for the next call, we will not include the current element, and move to the element which is not equal to
-	# the current element.
-	while idx < len(candidates) and candidates[idx] == currVal:
-		idx += 1
-	solve(candidates, target, idx, stack, result)
+    # Base case
+    if sum(stack) == target:
+        result.append(stack[:])
+        return
+    # Base case
+    if idx >= len(candidates) or sum(stack) > target:
+        return
+    # Getting the current element
+    currVal = candidates[idx]
+    # Adding this element to the stack and moving to the next index.
+    stack.append(currVal)
+    solve(candidates, target, idx + 1, stack, result)
+    stack.pop()
+    # Now for the next call, we will not include the current element, and move to the element which is not equal to
+    # the current element.
+    while idx < len(candidates) and candidates[idx] == currVal:
+        idx += 1
+    solve(candidates, target, idx, stack, result)
--- Backtracking-Recursion/decode_string.py
+++ Backtracking-Recursion/decode_string.py
@@ -8,53 +8,87 @@ Output: "aaabcbc"
 
 Input: s = "3[a2[c]]"
 Output: "accaccacc"
+
+https://leetcode.com/problems/decode-string/description/
+
+Brute Force:
+Core Idea:
+Think of the problem as nested subproblems
+Each time we see a '[', we need to solve what's inside before multiplying
+Use recursion to handle these nested parts
+
+Key Principles:
+When we hit a '[', make a recursive call to process everything inside until matching ']'
+Keep track of current position in string using an index
+Build numbers digit by digit before '['
+Return both the decoded substring and next position after ']'
+
+Time Complexity: O(n * k) where n is the length of the input string and k is the maximum value of the multiplier
+Space Complexity: O(n) for the recursion stack
+
+Optimized:
+Core Idea:
+Use stack to remember "outer" context while processing "inner" brackets
+Stack stores tuples of (string_so_far, number_of_repetitions)
+Build result incrementally without recursion
+
+Key Principles:
+Stack represents the "state" before entering each '['
+When we see '[': push current state, start fresh
+When we see ']': pop previous state, multiply current string
+Numbers and letters are processed as we see them
+
+Time = Space = O(n)
+
 """
 
 
-def decodeString(s: str) -> str:
-	# Time = Space = O(n)
-	"""
-	Logic:
-	We will iterate over every char. If the char is an alphabet then we will store in the res list,
-	if the char is a digit, then we will parse all the digits which follow until we hit a "[", at this point we
-	will do recursion and the return will be a string which we will multiply with the number which we parsed.
-	Now, if we hit a case where we find the char as closing bracket "]", we will break and return the result
+def brute_force(s: str) -> str:
+    def decode_helper(s: str, i: int) -> tuple[str, int]:
+        result = []
+        num = 0
 
-	We will also track the visited indices.
-	"""
-	# Tracking the visited indices
-	visited = set()
-	return solve(s, visited, 0)
+        while i < len(s):
+            char = s[i]
+
+            if char.isdigit():
+                num = num * 10 + int(char)
+            elif char == "[":
+                # Recursive call to handle nested brackets
+                substring, next_i = decode_helper(s, i + 1)
+                result.append(substring * num)
+                num = 0
+                i = next_i
+            elif char == "]":
+                return "".join(result), i
+            else:
+                result.append(char)
+            i += 1
+
+        return "".join(result), i
+
+    decoded_string, _ = decode_helper(s, 0)
+    return decoded_string
 
 
-def solve(s, visited, idx):
-	# Stores result for current layer
-	res = []
+def optimized_stack(s):
+    stack = []
+    current_string = ""
+    current_number = 0
 
-	while idx < len(s):
-		# If this index has not been visited.
-		if idx not in visited:
-			# Visited it
-			visited.add(idx)
-			# If this char is an alphabet
-			if s[idx].isalpha():
-				# Append to the result
-				res.append(s[idx])
-			elif s[idx].isdigit():
-				# If its a digit, then we will parse all the digits which may come after it.
-				num = 0
-				while idx < len(s) and s[idx].isdigit():
-					# Mark as visited.
-					visited.add(idx)
-					num = num * 10 + int(s[idx])
-					idx += 1
-				idx -= 1
-				# Now we want to multiple the bracket's content with the above parsed number, so we make a recursive
-				# call, and then add it to the result.
-				res.append(solve(s, visited, idx + 1) * num)
-			elif s[idx] == ']':
-				# If we hit a closing bracket, we will break
-				break
-		idx += 1
-	# returning the resultant string
-	return ''.join(res)
+    for char in s:
+        if char.isdigit():
+            current_number = current_number * 10 + int(char)
+        elif char == "[":
+            # Push the current state to stack
+            stack.append((current_string, current_number))
+            current_string = ""
+            current_number = 0
+        elif char == "]":
+            # Pop the previous state
+            prev_string, num = stack.pop()
+            current_string = prev_string + current_string * num
+        else:
+            current_string += char
+
+    return current_string
