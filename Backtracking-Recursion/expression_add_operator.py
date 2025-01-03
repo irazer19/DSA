861cadb11f4e3c50d5350a7aa7a4b355e7e3e51e
Saurav Gupta <irazer19@gmail.com>
2025-01-03 09:42:55 +0530

    update

--- Backtracking-Recursion/expression_add_operator.py
+++ Backtracking-Recursion/expression_add_operator.py
@@ -7,6 +7,9 @@ Note that operands in the returned expressions should not contain leading zeros.
 Input: num = "123", target = 6
 Output: ["1*2*3","1+2+3"]
 Explanation: Both "1*2*3" and "1+2+3" evaluate to 6.
+
+https://leetcode.com/problems/expression-add-operators/description/
+
 """
 
 
@@ -36,21 +39,21 @@ def expressionAddOperator(num, target):
 def dfs(num, target, idx, stack, curSum, prev, result):
     # If the idx is out of bounds and the current sum = target, then we add the stack's expression to the result.
     if idx == len(num) and curSum == target:
-        result.append(''.join(stack))
+        result.append("".join(stack))
         return
 
     # Starting from given idx, we slice the num to get different length number,
     # example: 1(first iteration), then 12 (2nd iteration), then 123 (3rd iteration).
     for i in range(idx, len(num)):
         # Getting the sliced number and converting to integer.
-        val = int(num[idx: i + 1])
+        val = int(num[idx : i + 1])
         # If this is the first number, then we cannot add an operator before it
         if idx == 0:
             # i + 1 = Moving to the next index of num
             # stack + [num[idx: i + 1]] = Appending newly sliced number to the stack
             # currSum = val, because for the first number, the total sum is this number itself.
             # prev = val, for the next call, the current value is the previous value.
-            dfs(num, target, i + 1, stack + [num[idx: i + 1]], val, val, result)
+            dfs(num, target, i + 1, stack + [num[idx : i + 1]], val, val, result)
         else:
             # stack + ['+'] + [num[idx: i + 1]] = Appending newly sliced number to the stack along with the operator.
             # For addition and subtraction, curSum = curSum +/- val, but for multiplication it is tricky,
@@ -59,9 +62,33 @@ def dfs(num, target, idx, stack, curSum, prev, result):
             # Here, the currSum = 5, prev = 3, and val = 4. Normally, we get the currSum = 5 * 4 = 20 which is wrong.
             # So that's why we do (val(4) * prev(3)) + 2, we get 2 using (currSum - prev).
             # And therefore, the "prev" for multiplication = val * prev, because of the BODMAS rule.
-            dfs(num, target, i + 1, stack + ['+'] + [num[idx: i + 1]], curSum + val, val, result)
-            dfs(num, target, i + 1, stack + ['-'] + [num[idx: i + 1]], curSum - val, -val, result)
-            dfs(num, target, i + 1, stack + ['*'] + [num[idx: i + 1]], curSum - prev + val * prev, val * prev, result)
+            dfs(
+                num,
+                target,
+                i + 1,
+                stack + ["+"] + [num[idx : i + 1]],
+                curSum + val,
+                val,
+                result,
+            )
+            dfs(
+                num,
+                target,
+                i + 1,
+                stack + ["-"] + [num[idx : i + 1]],
+                curSum - val,
+                -val,
+                result,
+            )
+            dfs(
+                num,
+                target,
+                i + 1,
+                stack + ["*"] + [num[idx : i + 1]],
+                curSum - prev + val * prev,
+                val * prev,
+                result,
+            )
 
 
-print(expressionAddOperator('232', 8))
+print(expressionAddOperator("232", 8))
